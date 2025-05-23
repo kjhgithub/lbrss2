1a3d4350dd1615a62fbe4ce272617988a32f9b69
kjhgithub <kim-habermehl@mailbox.org>
2025-05-24 00:25:47 +0200

    Layout and Links part 2-2

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import logo_long_blank from "@/assets/images/logo_long_blank.png";
 import LanguageSwitcher from "./LanguageSwitcher";
+import { Suspense } from "react";
 
 function Header() {
   return (
     <header className="flex flex-col items-center justify-center gap-2 mb-10">
-      <LanguageSwitcher />
+      <Suspense fallback={<span>Loading....</span>}>
+        <LanguageSwitcher />{" "}
+      </Suspense>
       <Link href="/">
         <img src={logo_long_blank.src} alt="Logo" className="h-28" />
       </Link>
--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 export function LanguageSwitcher() {
   const router = useRouter();
@@ -14,23 +13,21 @@ export function LanguageSwitcher() {
   };
 
   return (
-    <Suspense fallback={<span>Loading....</span>}>
-      <div className="flex items-center justify-center gap-2">
-        <button
-          className="cursor-pointer hover:underline"
-          onClick={() => setLang("de")}
-        >
-          DE 🇩🇪
-        </button>
-        <div className="">|</div>
-        <button
-          className="cursor-pointer hover:underline"
-          onClick={() => setLang("en")}
-        >
-          🇬🇧 EN
-        </button>
-      </div>
-    </Suspense>
+    <div className="flex items-center justify-center gap-2">
+      <button
+        className="cursor-pointer hover:underline"
+        onClick={() => setLang("de")}
+      >
+        DE 🇩🇪
+      </button>
+      <div className="">|</div>
+      <button
+        className="cursor-pointer hover:underline"
+        onClick={() => setLang("en")}
+      >
+        🇬🇧 EN
+      </button>
+    </div>
   );
 }
 
