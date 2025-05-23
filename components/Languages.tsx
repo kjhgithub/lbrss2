ca994ffe764a1f306b38aad1220e38ecbb8cdcd1
kjhgithub <kim-habermehl@mailbox.org>
2025-05-23 23:01:58 +0200

    Layout changes mit Suspense

--- components/Languages.tsx
+++ components/Languages.tsx
@@ -1,17 +1,19 @@
 "use client";
-import { useSearchParams, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { Suspense } from "react";
 import clsx from "clsx";
 
 function Languages() {
-  const searchParams = useSearchParams();
   const pathname = usePathname();
 
   return (
-    <div className="flex items-center justify-center gap-2">
-      <div className={clsx({ underline: pathname === "/" })}>DE 🇩🇪</div>
-      <div className="">|</div>
-      <div className="">🇬🇧 EN</div>
-    </div>
+    <Suspense fallback={<span>Loading....</span>}>
+      <div className="flex items-center justify-center gap-2">
+        <div className={clsx({ underline: pathname === "/" })}>DE 🇩🇪</div>
+        <div className="">|</div>
+        <div className="">🇬🇧 EN</div>
+      </div>
+    </Suspense>
   );
 }
 
